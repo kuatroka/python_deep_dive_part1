8c35a61669139f2303d47abef6aee9476e300758
kuatroka <aaarmani_2000@yahoo.com>
2019-10-29 18:55:47 +0000

    comparison operators

--- part1.py
+++ part1.py
@@ -2191,14 +2191,50 @@ age = 34
 min < age < max
 
 
+#######################################################
+### Functions
+
+def my_func(a, b, c):
+    print('a={0}, b={1}, c={2}'.format(a, b, c))
+
+my_func(1,2,3)
+
+def my_func(a, b=2, c=2):
+    print('a={0}, b={1}, c={2}'.format(a, b, c))
+
+my_func(10, 20, 30)
+
+my_func(10, 20)
+
+my_func(10)
+
+def my_func(a=1, b=2, c=3):
+    print('a={0}, b={1}, c={2}'.format(a, b, c))
+
+my_func()
 
 
+###########################################################
+####### keyword parameters
 
+def my_func(a, b=2, c=3):
+    print('a={0}, b={1}, c={2}'.format(a, b, c))
 
+my_func(c=30, b=20, a=10) # even with wrong oder, it works correctly
+## because we use named arguments here
 
+my_func(10, c=20, b=10)
 
+ my_func(10, c=30)
 
+##########################################################
+#### Upacking tuples
 
+a = (1,2,3)
+type(a)
+
+a = 1, 2, 3
+type(a)
 
 
 
